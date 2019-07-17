d7374108e4d28a84be0af3671b06e6936a6d8e78
Alexander Ershov <rufflet@ya.ru>
2019-07-17 22:34:55 +0300

    minor

---
+++ components/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <div>LOGO</div>
+    <div>MENU</div>
+  </header>
+</template>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
